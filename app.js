b1354600ef44087f2a10344466f4108aa861fa8c
Victor Amaral <victoramaral@usp.br>
2021-07-17 19:31:08 -0300

    habilitando CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 require("dotenv-safe").config()
 const app = express()
+const cors = require('cors')
 const port = 3000
 
 const jwt = require('jsonwebtoken')
 const Aluno = require('./models/aluno.js')
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('<h1 style="color : blue;"> MasterOfNotas Back-End! <\h1>')
@@ -16,16 +18,16 @@ app.get('/', (req, res) => {
 app.post('/auth', async (req, res) => {
   user = req.body.username
   passwd = req.body.password
-  const query_response = await Aluno.findOne({where: {login: user, password: passwd} })
+  const query_response = await Aluno.findOne({ where: { login: user, password: passwd } })
 
-  if(query_response == null)
-    res.status(500).json({message: "Login Inválido!"})
-  else{
-      const id = query_response.id;
-      const token = jwt.sign({id}, process.env.SECRET, {
-        expiresIn: 1200 // 20 min
-      })
-      res.json({auth: true, token: token, username: query_response.name})
+  if (query_response == null)
+    res.status(500).json({ message: "Login Inválido!" })
+  else {
+    const id = query_response.id;
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 1200 // 20 min
+    })
+    res.json({ auth: true, token: token, username: query_response.name })
   }
 })
 
