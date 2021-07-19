d6a4af6588ecc0c59f65c4286716b00f2e821c77
Helbert M Pinto <helbert.tec.info@gmail.com>
2021-07-18 21:08:04 -0300

    fix ids

--- routes/professor/set-alunos-disciplina.js
+++ routes/professor/set-alunos-disciplina.js
@@ -14,29 +14,29 @@ module.exports = function (app) {
       return
     }
 
-    const { disciplineId: disciplinaId, alunos } = req.body
-    if (!disciplinaId || !alunos) {
+    const { disciplineId: disciplinaId, alunosId } = req.body
+    if (!disciplinaId || !alunosId) {
       res.status(400).json({ message: "bad request" })
       return
     }
 
     const disciplina = await Disciplina.findByPk(disciplinaId)
-    if (disciplina.emailProfessor !== professor.email) {
+    if (!disciplina || disciplina.professorId !== professor.id) {
       res.status(401).json({ message: "access danied" })
       return
     }
 
     const alunosAtual = await AlunoDisciplina.findAll({ raw: true, where: { disciplinaId } })
 
-    const alunosAdd = alunos.filter(aluno => !alunosAtual.find(alunoAtual => aluno.email === alunoAtual.emailAluno))
-    const alunosRemove = alunosAtual.filter(alunoAtual => !alunos.find(aluno => aluno.email === alunoAtual.emailAluno))
+    const alunosAdd = alunosId.filter(alunoId => !alunosAtual.find(alunoAtual => +alunoId === +alunoAtual.alunoId))
+    const alunosRemove = alunosAtual.filter(alunoAtual => !alunosId.includes(+alunoAtual.alunoId))
 
     sequelize.transaction(async (t) => {
-      const alunosAddObj = alunosAdd.map(aluno => ({ disciplinaId, emailAluno: aluno.email }))
+      const alunosAddObj = alunosAdd.map(alunoId => ({ disciplinaId, alunoId }))
       await AlunoDisciplina.bulkCreate(alunosAddObj, { transaction: t })
 
-      const alunosEmailRemove = alunosRemove.map(aluno => aluno.emailAluno)
-      await AlunoDisciplina.destroy({ where: { disciplinaId, emailAluno: alunosEmailRemove } }, { transaction: t })
+      const alunosEmailRemove = alunosRemove.map(aluno => aluno.alunoId)
+      await AlunoDisciplina.destroy({ where: { disciplinaId, alunoId: alunosEmailRemove } }, { transaction: t })
 
       res.json({ status: 'OK' })
 
