b186818bc3a1e5fb17236b1fcc28dc1d0a14c019
Victor Amaral <victoramaral@usp.br>
2021-07-29 23:53:58 -0300

    mudar para variável de ambiente

--- db-index.js
+++ db-index.js
@@ -1,7 +1,40 @@
 
 const Sequelize = require('sequelize')
-const dbconfig = require('./config/config.json')
 const sequelizeModels = require('./models/')
+
+
+
+const dbconfig = {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      keepAlive: true
+    },
+    ssl: true,
+    logging: false,
+    seederStorage: "sequelize",
+    define: {
+      timestamps: true,
+      underscored: true
+    }
+}
+
+
+
+
+
+
+
+
+
 
 const connection = new Sequelize(dbconfig)
 
